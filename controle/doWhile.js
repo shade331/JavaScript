d3332116a7af34400e99ea6d2f5c7e68224f84f9
Fabio <71786875+shade331@users.noreply.github.com>
2024-02-28 21:08:50 -0300

    .

--- controle/doWhile.js
+++ controle/doWhile.js
@@ -5,8 +5,8 @@ function getInteiroAleatorioEntre(min, max) {
 
 let opcao = 0
 
-while (opcao != -1) {
+do {
     opcao = getInteiroAleatorioEntre(-1, 10)
     console.log(`Opção escolhida foi ${opcao}.`)
-}
+} while (opcao != -1)
 console.log('Até a próxima!')
